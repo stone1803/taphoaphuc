2e44532bee39a6bc81994348efcbcccb42eba237
Phuc le <jangmi119085@gmail.com>
2020-10-27 19:12:29 +0700

    sadapsadsada

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,64 @@ import Head from "next/head";
 import Main from "./layout/main";
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
-import Layout from "./layout/mylayout";
-import Test from "./sanpham/test";
 
+import Layout from "./layout/mylayout"
+import IndexProduct from "./sanpham";
 export default function Home() {
   return (
     <div>
-       <DefaultSeo {...SEO}
-       
+        <Head>
+        <title>Lão Bạch Shop</title>
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossorigin="anonymous"
+        ></link>
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+          integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+          integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+          crossorigin="anonymous"
+        ></script>{" "}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossorigin="anonymous"
+        ></link>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CKQ5QZF0QR"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-CKQ5QZF0QR', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+          }}
         />
-     
-    <Layout>
-      <Main/>
-      <Test/>
-    </Layout>
+      </Head>
+          <IndexProduct/>
     </div>
   );
 }
