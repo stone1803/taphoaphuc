46dfdcb39dfa30f2e70084f6b443bbcb144dd47c
Phuc le <jangmi119085@gmail.com>
2020-11-01 12:21:34 +0700

    sdsah3032r2

---
+++ pages/lienhe/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from "../layout/mylayout"
+export default function LienHeIndex() {
+    return (
+        <Layout>
+            Mọi thắc mắc vui lòng liên hệ số điện thoại : 0937895335 A phúc .
+        </Layout>
+    )
+}
---
+++ pages/next-sitemap.js
@@ -0,0 +1,12 @@
+  
+module.exports = {
+    siteUrl: 'https://laobach.com',
+    generateRobotsTxt: true,
+    // optional
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://laobach.com/sitemap.xml',
+
+      ],
+    },
+  }
