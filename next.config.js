70aff91f315d374cc9d456aecf6f1f892cce457c
stone1803 <jangmi110985@gmail.com>
2020-11-03 11:02:38 +0700

    dsads

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@ const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
   /* config options here */
 })
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  }
+};
